8215f90edbaf6ec6f955bb6567965e8eb9e608a4
fnandesrafael <fnandesrafael@pm.me>
2023-08-05 14:52:21 -0300

    feat: import Header to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 
 import * as S from './global.styles';
 
 function App() {
-  return <S.AppWrapper></S.AppWrapper>;
+  return (
+    <S.AppWrapper>
+      <Header />
+    </S.AppWrapper>
+  );
 }
 
 export default App;
