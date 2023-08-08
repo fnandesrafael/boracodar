67ab3bf83b35986d19628c5818dcb2449be3ec8f
fnandesrafael <fnandesrafael@pm.me>
2023-08-08 08:47:58 -0300

    fix: import hierarchy on main.jsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import MessagesProvider from './context/MessagesContext.tsx';
 
 import * as S from './global.styles';
-import MessagesProvider from './context/MessagesContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
